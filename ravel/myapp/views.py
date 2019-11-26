f5ebf81cb068318dc5e0a3de65e4c99310838e03
akillkumar <akhil.kumar_ug21@ashoka.edu.in>
2019-11-26 08:11:33 -0800

    User login functionality

--- ravel/myapp/views.py
+++ ravel/myapp/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Flight
+from .models import Flight, Trains
 
 def index (request):
 	flight_list = Flight.objects.order_by('flight_number')[:5]
 	output = ', '.join([f.flight_name for f in flight_list])
 
+	train_list = Trains.objects.order_by('train_number')[:5]
+	output = ', '.join([t.train_name for t in train_list])
+
+
 	template = loader.get_template('myapp/index.html')
 	context = {
-		'flight_list' : flight_list, 
+		'flight_list' : flight_list,
+		'train_list' : train_list, 
 	}
 	return render (request, 'myapp/index.html', context)
 
--- ravel/ravel/settings.py
+++ ravel/ravel/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp.apps.MyappConfig',
+    'myapp.apps.MyappConfig',      
+    'register.apps.RegisterConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/myapp'
