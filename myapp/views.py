e8bec7024a1dd80aa6003b1e3314e6228bc87a71
dhruvapanyam <dhruva.origami@gmail.com>
2019-11-30 23:48:14 +0530

    Frontend up, filtering started

--- myapp/views.py
+++ myapp/views.py
@@ -25,16 +25,64 @@ def myFunc (request):
 	return render (request, 'myapp/temp.html',{'origin':origin,'dest':dest})
 
 def filter_flights (request):
+	global myVar
 	if request.method=='GET':
 		origin=request.GET.get('origin','')
 		dest = request.GET.get('dest','')
+		dept = request.GET.get('dept','')
+		res = datetime.strptime(dept,'%m/%d/%Y')
+		dates = {}
+		myVar +=1
+		print('my var = ', myVar)
+		for i in range(6):
+			dates[i] = res + dt.timedelta(days=i-2)
+		
 		passengers = request.GET.get('passengers','')
 
-		print('pass = ',passengers)
+		
 
-		results = Flight.objects.filter(origin__icontains = origin,dest__icontains = dest, seats__gte = passengers)
+		sort = request.GET.get('sort','')
+		deptTime = request.GET.get('deptTime','')
 
-	return render (request, 'myapp/temp.html', {'flight_list': results, 'origin':origin,'dest':dest})
+		results = Flight.objects.filter(
+			origin__icontains = origin,
+			dest__icontains = dest,
+			dept__year = res.year,
+			dept__month = res.month,
+			dept__day = res.day, 
+			seats__gte = passengers)
+		
+		if(sort):
+			if(sort=='price'):
+				order='price'
+
+			if(sort=='-price'):
+				order='-price'
+
+			results.order_by(order)
+		
+		if deptTime:
+
+			print('YEEEEEEEE')
+			
+			if deptTime=='1' or deptTime==1:
+				print('NEEEEEEEEEEe')
+				results.filter(dept__hour__lt = 10)
+			
+
+		
+	return render (request, 'myapp/search.html', 
+		{
+			'flight_list': results, 
+			'origin':origin,
+			'dest':dest,
+			'dept':dept,
+			'passengers': passengers,
+			'dates':dates,
+			'res':res,
+			'sort':sort
+		})
+
 
 def filter_trains (request):
 	if request.method=='GET':
