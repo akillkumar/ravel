90402ecd4433faefd31b3f636517987b9ae4dfee
Akhil <akillkumar@users.noreply.github.com>
2019-12-03 15:55:15 +0530

    Deleted some shiz

--- ravel/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Flight, Trains, Buses, Hotel
-
-admin.site.register(Flight)
-admin.site.register(Trains)
-admin.site.register(Buses)
-admin.site.register(Hotel)
